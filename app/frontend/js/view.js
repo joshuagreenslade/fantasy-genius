1b8819e0b96c8a1f5a162ea1de535d1cfbb55279
VethushaanAkilathasan <vethush@gmail.com>
2017-03-18 15:47:28 -0400

    frontend update

--- app/frontend/js/view.js
+++ app/frontend/js/view.js
@@ -729,13 +729,13 @@ var view = (function() {
 	};
 
 	view.displayusers = function(data){
-		var userdisplay = document.getElementById("leagueusers");
+		var userdisplay = document.getElementById("users");
 		userdisplay.innerHTML = "";
 		data.forEach(function(user){
-			var e = document.createElement("div");
+			var e = document.createElement("tr");
 			e.classname = "list-group";
 			e.id = user.owner;
-			e.innerHTML = `<a href="#" class="list-group-item">${user.owner}</a>`;
+			e.innerHTML = `<td>${user.owner}</td><td>${user.score}</td>`;
 			userdisplay.append(e);
 			e.onclick = function(e){
 				e.preventDefault();
